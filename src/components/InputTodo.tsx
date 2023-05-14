ecd43004bc53c6a1b48858535dfe5856ca8421a1
xxyeon129 <xxyeon129@gmail.com>
2023-05-15 01:03:50 +0900

    Design: input 과제 조건 UI 구현

--- src/components/InputTodo.tsx
+++ src/components/InputTodo.tsx
@@ -1,8 +1,10 @@
-import { FaPlusCircle, FaSpinner } from 'react-icons/fa';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+import { FiSearch } from 'react-icons/fi';
 import { useCallback, useEffect, useState } from 'react';
 import { TodoTypes } from '../types/todo';
 import { createTodo } from '../api/todo';
 import useFocus from '../hooks/useFocus';
+import styled from 'styled-components';
 
 interface InputTodoType {
   setTodos: React.Dispatch<React.SetStateAction<TodoTypes[]>>;
@@ -46,24 +48,79 @@ const InputTodo = ({ setTodos }: InputTodoType) => {
   );
 
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <input
-        className="input-text"
-        placeholder="Add new todo..."
-        ref={ref}
-        value={inputText}
-        onChange={e => setInputText(e.target.value)}
-        disabled={isLoading}
-      />
-      {!isLoading ? (
-        <button className="input-submit" type="submit">
-          <FaPlusCircle className="btn-plus" />
-        </button>
-      ) : (
-        <FaSpinner className="spinner" />
-      )}
-    </form>
+    <StFormContainer>
+      <StForm onSubmit={handleSubmit}>
+        <StSearchIconWrapper>
+          <FiSearch />
+        </StSearchIconWrapper>
+        <StInput
+          className="input-text"
+          placeholder="Placeholder"
+          ref={ref}
+          value={inputText}
+          onChange={e => setInputText(e.target.value)}
+          disabled={isLoading}
+        />
+        {isLoading && (
+          <StSpinnerContainer>
+            <AiOutlineLoading3Quarters className="spinner" />
+          </StSpinnerContainer>
+        )}
+      </StForm>
+    </StFormContainer>
   );
 };
+
+const StFormContainer = styled.div`
+  height: 67px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const StForm = styled.form`
+  width: 100%;
+  display: flex;
+  border: 1px solid #dedede;
+  border-radius: 6px;
+  transition: border 0.3s;
+  position: relative;
+
+  &:hover {
+    border-width: 3px;
+  }
+  &:focus-within {
+    border: 1px solid #9f9f9f;
+  }
+  padding-left: 15px;
+  padding-right: 16px;
+`;
+
+const StInput = styled.input`
+  width: 100%;
+  text-overflow: ellipsis;
+
+  &::placeholder {
+    color: #9f9f9f;
+  }
+`;
+
+const StSearchIconWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-right: 12px;
+  color: #7d7d7d;
+`;
+
+const StSpinnerContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 8px;
+  .spinner {
+    color: #4b4b4b;
+    animation: spin 1s infinite linear;
+  }
+`;
 
 export default InputTodo;
