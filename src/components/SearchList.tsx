81f8f7a0f8364148c0cc344ffb667451e86eea18
Jinnie <hjk7639@gmail.com>
2023-05-16 02:32:51 +0900

    feat: 연관 검색어 아이템 컴포넌트로 input setState와 연관 검색어를 추가하는 addKeywordTodo 함수 전달

--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -1,14 +1,22 @@
 import SearchListItem from './SearchListItem';
-import { SearchListBox } from './SearchList.styled';
-
+import { SearchListBox } from '../style/SearchList.styled';
 interface SearchListType {
   searchKeywordList: string[];
   focusRef: React.RefObject<HTMLUListElement>;
   focusIndex: number;
   inputText: string;
+  addKeywordTodo: (keyword: string) => Promise<void>;
+  setInputText: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const SearchList = ({ searchKeywordList, focusRef, focusIndex, inputText }: SearchListType) => {
+const SearchList = ({
+  searchKeywordList,
+  focusRef,
+  focusIndex,
+  inputText,
+  addKeywordTodo,
+  setInputText,
+}: SearchListType) => {
   return (
     <SearchListBox ref={focusRef}>
       {searchKeywordList.map((searhKeyword, idx) => (
@@ -16,6 +24,8 @@ const SearchList = ({ searchKeywordList, focusRef, focusIndex, inputText }: Sear
           searchKeyword={searhKeyword}
           isFocus={idx === focusIndex ? true : false}
           inputText={inputText}
+          setInputText={setInputText}
+          addKeywordTodo={addKeywordTodo}
         />
       ))}
     </SearchListBox>
