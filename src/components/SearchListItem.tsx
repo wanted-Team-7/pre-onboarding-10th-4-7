6668e75a27e547bfbbe13357e17341bb133e521a
Jinnie <hjk7639@gmail.com>
2023-05-16 02:43:23 +0900

    feat: 연관 검색어 아이템을 클릭하면 Todo리스트에 추가

--- src/components/SearchListItem.tsx
+++ src/components/SearchListItem.tsx
@@ -1,20 +1,33 @@
 import { useEffect, useRef } from 'react';
-import { SearchItem } from './SearchList.styled';
-
+import { SearchItem } from '../style/SearchList.styled';
 interface SearchListItemTyep {
   searchKeyword: string;
   isFocus: boolean;
   inputText: string;
+  addKeywordTodo: (keyword: string) => Promise<void>;
+  setInputText: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const SearchListItem = ({ searchKeyword, isFocus, inputText }: SearchListItemTyep) => {
+const SearchListItem = ({
+  searchKeyword,
+  isFocus,
+  inputText,
+  addKeywordTodo,
+  setInputText,
+}: SearchListItemTyep) => {
   const focusRef = useRef<HTMLLIElement>(null);
   useEffect(() => {
     focusRef.current?.scrollIntoView({ behavior: 'smooth', block: 'end' });
   }, [isFocus]);
 
+  const selectRelativeKeyword = (e: React.MouseEvent<HTMLLIElement>) => {
+    const data = e.currentTarget.innerText;
+    addKeywordTodo(data);
+    setInputText('');
+  };
+
   return (
-    <SearchItem isFocus={isFocus} ref={focusRef}>
+    <SearchItem isFocus={isFocus} ref={focusRef} onClick={selectRelativeKeyword}>
       {searchKeyword.split(inputText)[0]}
       <span>{inputText}</span>
       {searchKeyword.split(inputText)[1]}
