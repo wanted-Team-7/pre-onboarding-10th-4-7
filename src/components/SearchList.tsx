24ebd637aa5ef92add912c6f60d297c0e0ef50d8
Jinnie <hjk7639@gmail.com>
2023-05-16 04:11:21 +0900

    feat: 연관 검색어 검색 중 isLoading이 true일 때 스피너 표시

--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -1,10 +1,12 @@
 import SearchListItem from './SearchListItem';
+import { FaSpinner } from 'react-icons/fa';
 import { SearchListBox } from '../style/SearchList.styled';
 interface SearchListType {
   searchKeywordList: string[];
   focusRef: React.RefObject<HTMLUListElement>;
   focusIndex: number;
   inputText: string;
+  isLoading: boolean;
   addKeywordTodo: (keyword: string) => Promise<void>;
   setInputText: React.Dispatch<React.SetStateAction<string>>;
 }
@@ -14,20 +16,27 @@ const SearchList = ({
   focusRef,
   focusIndex,
   inputText,
+  isLoading,
   addKeywordTodo,
   setInputText,
 }: SearchListType) => {
   return (
     <SearchListBox ref={focusRef}>
-      {searchKeywordList.map((searhKeyword, idx) => (
-        <SearchListItem
-          searchKeyword={searhKeyword}
-          isFocus={idx === focusIndex ? true : false}
-          inputText={inputText}
-          setInputText={setInputText}
-          addKeywordTodo={addKeywordTodo}
-        />
-      ))}
+      {isLoading ? (
+        <FaSpinner className="spinner" />
+      ) : (
+        searchKeywordList.map((searhKeyword, idx) => (
+          <SearchListItem
+            key={idx}
+            searchKeyword={searhKeyword}
+            isFocus={idx === focusIndex ? true : false}
+            inputText={inputText}
+            setInputText={setInputText}
+            addKeywordTodo={addKeywordTodo}
+          />
+        ))
+      )}
+      {searchKeywordList.length === 0 && <span>검색 결과가 없습니다.</span>}
     </SearchListBox>
   );
 };
