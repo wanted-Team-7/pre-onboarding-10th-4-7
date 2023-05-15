4bf1b86e18831c976e333590f80a40390a744b87
Withlaw <car2559@naver.com>
2023-05-15 16:50:03 +0900

    design: 드롭다운 아이템 클릭시 색상 변화

--- src/components/TodoSearchResult.tsx
+++ src/components/TodoSearchResult.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
 import { TodoDispatchContext } from '../pages/Main';
 import { createTodo } from '../api/todo';
@@ -8,9 +8,11 @@ interface ITodoSearchResult {
 }
 
 function TodoSearchResult({ value }: ITodoSearchResult) {
+  const [isClicked, setIsClicked] = useState(false);
   const dispatch = useContext(TodoDispatchContext);
   const handleClick = async () => {
     try {
+      setIsClicked(true);
       const { data } = await createTodo({ title: value });
       if (data) {
         dispatch?.setTodoListData(prevTodos => [...prevTodos, data]);
@@ -24,7 +26,7 @@ function TodoSearchResult({ value }: ITodoSearchResult) {
   };
 
   return (
-    <ItemContainer onClick={handleClick}>
+    <ItemContainer onClick={handleClick} isClicked={isClicked}>
       {value.length > 40 ? value.slice(0, 39) + '...' : value}
     </ItemContainer>
   );
@@ -32,7 +34,7 @@ function TodoSearchResult({ value }: ITodoSearchResult) {
   // return <ItemContainer>hasdfkalsdnfhbkjashdfnkasdfhklsdfadsfasdfalhsdnfjlkbabs</ItemContainer>;
 }
 
-const ItemContainer = styled.div`
+const ItemContainer = styled.div<{ isClicked: boolean }>`
   /* font color #2BC9BA  */
   width: 100%;
   height: 28px;
@@ -50,7 +52,18 @@ const ItemContainer = styled.div`
 
   &:hover {
     background-color: #f2f2f2;
+    cursor: pointer;
   }
+
+  ${props =>
+    props.isClicked &&
+    `
+      color: #2bc9ba;
+      background-color: #d5f4f1;
+      &:hover {
+        background-color: #d5f4f1;
+      }
+  `}
 `;
 
 export default TodoSearchResult;
