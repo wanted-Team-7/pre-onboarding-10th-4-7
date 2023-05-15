dded008bf8a3c543735a3ac6460c50d627d259f7
Jinnie <hjk7639@gmail.com>
2023-05-15 19:59:55 +0900

    feat: 연관 검색어 리스트 state 생성 & 연관 검색 api call 핸들러 함수 생성

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,25 @@
 import { useEffect, useState } from 'react';
-import { getTodoList } from '../api/todo';
-import { TodoTypes } from '../types/todo';
 import Header from '../components/Header';
 import InputTodo from '../components/InputTodo';
 import TodoList from '../components/TodoList';
+import SearchList from '../components/SearchList';
+import { getTodoList, searchTodoKeyword } from '../api/todo';
+import { TodoTypes } from '../types/todo';
 
 const Main = () => {
+  const [inputText, setInputText] = useState<string>('');
+  const [searchKeywordList, setSearchKeywordList] = useState<string[]>([]);
   const [todoListData, setTodoListData] = useState<TodoTypes[]>([]);
+
+  const getSearchKeywordHandler = async (input: string) => {
+    if (input.trim() === '') {
+      setSearchKeywordList([]);
+      return;
+    }
+
+    const { data } = await searchTodoKeyword({ keyword: input, page: 1 });
+    setSearchKeywordList(data.result);
+  };
 
   useEffect(() => {
     (async () => {
@@ -19,7 +32,13 @@ const Main = () => {
     <div className="container">
       <div className="inner">
         <Header />
-        <InputTodo setTodos={setTodoListData} />
+        <InputTodo
+          setTodos={setTodoListData}
+          inputText={inputText}
+          setInputText={setInputText}
+          getSearchKeywordHandler={getSearchKeywordHandler}
+        />
+        {searchKeywordList.length > 0 ? <SearchList searchKeywordList={searchKeywordList} /> : null}
         <TodoList todos={todoListData} setTodos={setTodoListData} />
       </div>
     </div>
