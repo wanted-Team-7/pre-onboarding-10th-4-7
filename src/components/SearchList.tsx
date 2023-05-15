399d18706f2c8108db6f7e15722a6a2acf56f066
Jinnie <hjk7639@gmail.com>
2023-05-15 21:17:37 +0900

    feat: 연관 검색어 리스트에 ref 전달 & 연관 검색어 리스트 아이템에 현재 focus index와 아이템 index비교하여 boolean값 전달

--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -1,14 +1,17 @@
+import SearchListItem from './SearchListItem';
 import { SearchListBox } from './SearchList.styled';
 
 interface SearchListType {
   searchKeywordList: string[];
+  focusRef: React.RefObject<HTMLUListElement>;
+  focusIndex: number;
 }
 
-const SearchList = ({ searchKeywordList }: SearchListType) => {
+const SearchList = ({ searchKeywordList, focusRef, focusIndex }: SearchListType) => {
   return (
-    <SearchListBox>
-      {searchKeywordList.map(searhKeyword => (
-        <li>{searhKeyword}</li>
+    <SearchListBox ref={focusRef}>
+      {searchKeywordList.map((searhKeyword, idx) => (
+        <SearchListItem searchKeyword={searhKeyword} isFocus={idx === focusIndex ? true : false} />
       ))}
     </SearchListBox>
   );
