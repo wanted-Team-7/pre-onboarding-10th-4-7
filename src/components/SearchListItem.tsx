e565e506e2f7c9fa503a3159f23acb9b26e51bcf
Jinnie <hjk7639@gmail.com>
2023-05-15 22:25:47 +0900

    feat: 연관 검색어 결과와 Todo input값이 같은 것 컬러 하이라이트 처리

--- src/components/SearchListItem.tsx
+++ src/components/SearchListItem.tsx
@@ -4,16 +4,20 @@ import { SearchItem } from './SearchList.styled';
 interface SearchListItemTyep {
   searchKeyword: string;
   isFocus: boolean;
+  inputText: string;
 }
 
-const SearchListItem = ({ searchKeyword, isFocus }: SearchListItemTyep) => {
+const SearchListItem = ({ searchKeyword, isFocus, inputText }: SearchListItemTyep) => {
   const focusRef = useRef<HTMLLIElement>(null);
   useEffect(() => {
     focusRef.current?.scrollIntoView({ behavior: 'smooth', block: 'end' });
   }, [isFocus]);
+
   return (
     <SearchItem isFocus={isFocus} ref={focusRef}>
-      {searchKeyword}
+      {searchKeyword.split(inputText)[0]}
+      <span>{inputText}</span>
+      {searchKeyword.split(inputText)[1]}
     </SearchItem>
   );
 };
