95b036dc2b05277265c370a1ed03834bece5d9c2
1yoouoo <1yoouoo@gmail.com>
2023-05-16 21:09:37 +0900

    fix: update index.tsx for react18 root

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import App from './App';
-import ReactDOM from 'react-dom';
-import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(<App />);
