2fb5a4dbe7b45d5cda8e427db8985b9782837c18
junyong <49552804+leejy001@users.noreply.github.com>
2023-05-17 00:01:31 +0900

    refactor: 두 검색 기능 하나로 병합

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,4 @@
 import { TodoTypes, getTodoList } from '../api/todo';
-
 import { ChangeEvent, useCallback, useEffect, useRef, useState } from 'react';
 import { createTodo, searchTodo } from '../api/todo';
 import useDebounce from '../hooks/useDebounce';
@@ -36,10 +35,10 @@ const Main = () => {
   const [isNoMoreData, setIsNoMoreData] = useState<boolean>(true);
 
   // currentPage: 현재 페이지
-  const [currentPage, setCurrentPage] = useState<number>(1);
+  // const [currentPage, setCurrentPage] = useState<number>(1);
 
   // total: 전체 데이터 개수
-  const [total, setTotal] = useState<number>(0);
+  // const [total, setTotal] = useState<number>(0);
 
   // observer: IntersectionObserver 객체
   const observer = useRef<IntersectionObserver | null>(null);
@@ -47,28 +46,53 @@ const Main = () => {
   // debouncedSearchQuery: 디바운스 적용된 검색어
   const debouncedSearchQuery = useDebounce(inputText, DEBOUNCED_DELAY);
 
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const handleSearchData = useCallback(
+    async (type: string, debouncedSearchQuery: string) => {
+      if (!debouncedSearchQuery) {
+        setSearchedResponse([]);
+        return;
+      }
+      if (type === 'first') {
+        setCurrentPage(1);
+        setSearchedResponse([]);
+      }
+      if (type === 'scroll') setCurrentPage((prevPage: number) => prevPage + 1);
+      const updateCurrentPage = type === 'scroll' ? currentPage + 1 : 1;
+      setIsMoreLoading(true);
+      const { data } = await searchTodo({ q: debouncedSearchQuery, page: updateCurrentPage });
+      setSearchedResponse((prevData: string[]) => [...prevData, ...data.result]);
+      setIsNoMoreData(data.page * data.limit >= data.total);
+      setIsMoreLoading(false);
+    },
+    [currentPage]
+  );
+
   // loadMoreData: 추가 데이터 로드 함수
-  const loadMoreData = useCallback(async () => {
-    setIsMoreLoading(true);
-    const newData = await searchTodo({ q: debouncedSearchQuery, page: currentPage + 1 });
-    setSearchedResponse((prevData: string[]) => [...prevData, ...newData.data.result]);
-    setTotal(newData.data.total);
-    setCurrentPage((prevPage: number) => prevPage + 1);
-    setIsMoreLoading(false);
-  }, [currentPage, debouncedSearchQuery]);
+  // const loadMoreData = useCallback(async () => {
+  //   setIsMoreLoading(true);
+  //   const newData = await searchTodo({ q: debouncedSearchQuery, page: currentPage + 1 });
+  //   setSearchedResponse((prevData: string[]) => [...prevData, ...newData.data.result]);
+  //   setTotal(newData.data.total);
+  //   setCurrentPage((prevPage: number) => prevPage + 1);
+  //   setIsMoreLoading(false);
+  // }, [currentPage, debouncedSearchQuery]);
 
   // lastItemRef: 마지막 항목의 ref 콜백 함수
   const lastItemRef = useCallback(
     (node: HTMLDivElement | null) => {
       if (observer.current) observer.current.disconnect();
       observer.current = new IntersectionObserver(entries => {
-        if (entries[0].isIntersecting) {
-          loadMoreData();
+        if (entries[0].isIntersecting && !isNoMoreData && !isMoreLoading) {
+          // loadMoreData();
+          handleSearchData('scroll', debouncedSearchQuery);
         }
       });
       if (node) observer.current.observe(node);
     },
-    [loadMoreData]
+    // [loadMoreData]
+    [debouncedSearchQuery, handleSearchData, isMoreLoading, isNoMoreData]
   );
 
   // onChangeInput: 입력 값 변경 시 처리 함수
@@ -83,17 +107,17 @@ const Main = () => {
   const handleBlur = () => setIsFocused(false);
 
   // handleChange: 검색어 변경 시 처리 함수
-  const handleChange = useCallback(async () => {
-    if (!debouncedSearchQuery) {
-      setSearchedResponse([]);
-      setTotal(0);
-      return;
-    }
-    const response = await searchTodo({ q: debouncedSearchQuery });
-    setSearchedResponse(response.data.result);
-    setTotal(response.data.total);
-    setCurrentPage(1);
-  }, [debouncedSearchQuery]);
+  // const handleChange = useCallback(async () => {
+  //   if (!debouncedSearchQuery) {
+  //     setSearchedResponse([]);
+  //     setTotal(0);
+  //     return;
+  //   }
+  //   const response = await searchTodo({ q: debouncedSearchQuery });
+  //   setSearchedResponse(response.data.result);
+  //   setTotal(response.data.total);
+  //   setCurrentPage(1);
+  // }, [debouncedSearchQuery]);
 
   // handleSubmit: 폼 제출 시 처리 함수
   const handleSubmit = useCallback(
@@ -116,20 +140,24 @@ const Main = () => {
     [inputText]
   );
 
-  useEffect(() => {
-    // 검색 결과가 전체 데이터 개수와 동일하다면 더 이상 데이터가 없음
-    if (searchedResponse.length === total) {
-      setIsNoMoreData(true);
-    } else {
-      setIsNoMoreData(false);
-    }
-  }, [searchedResponse.length, total]);
+  // useEffect(() => {
+  //   // 검색 결과가 전체 데이터 개수와 동일하다면 더 이상 데이터가 없음
+  //   if (searchedResponse.length === total) {
+  //     setIsNoMoreData(true);
+  //   } else {
+  //     setIsNoMoreData(false);
+  //   }
+  // }, [searchedResponse.length, total]);
+
+  // useEffect(() => {
+  //   // 입력 중인지 여부 설정
+  //   setIsTyping(!!debouncedSearchQuery);
+  //   handleChange();
+  // }, [handleChange, debouncedSearchQuery]);
 
   useEffect(() => {
-    // 입력 중인지 여부 설정
-    setIsTyping(!!debouncedSearchQuery);
-    handleChange();
-  }, [handleChange, debouncedSearchQuery]);
+    handleSearchData('first', debouncedSearchQuery);
+  }, [debouncedSearchQuery]);
 
   useEffect(() => {
     // 할 일 목록 데이터 로드
