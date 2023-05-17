4f585c2b1503a3828166daa76d476283f4413588
junyong <49552804+leejy001@users.noreply.github.com>
2023-05-17 14:25:56 +0900

    feat: 캐시 구현 및 적용

--- src/hooks/useSearchData.tsx
+++ src/hooks/useSearchData.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useState } from 'react';
 import { searchTodo } from '../api/todo';
+import { getCache, setCache } from '../utils/cache';
 
 interface UseSearchDataProps {
   setSearchedResponse: React.Dispatch<React.SetStateAction<string[]>>;
@@ -14,6 +15,19 @@ function useSearchData({
 }: UseSearchDataProps) {
   const [currentPage, setCurrentPage] = useState<number>(1);
 
+  const getSearchData = async (updateCurrentPage: number, debouncedSearchQuery: string) => {
+    const cacheData = await getCache(debouncedSearchQuery + updateCurrentPage);
+    console.log(cacheData);
+    if (!cacheData) {
+      console.log('caching!!');
+      const { data } = await searchTodo({ q: debouncedSearchQuery, page: updateCurrentPage });
+      setCache(debouncedSearchQuery + updateCurrentPage, data, 60 * 1000);
+      return data;
+    }
+    console.log('cached!!');
+    return cacheData;
+  };
+
   const handleSearchData = useCallback(
     async (type: string, debouncedSearchQuery: string) => {
       if (debouncedSearchQuery.trim() === '') {
@@ -27,9 +41,9 @@ function useSearchData({
       if (type === 'scroll') setCurrentPage((prevPage: number) => prevPage + 1);
       const updateCurrentPage = type === 'scroll' ? currentPage + 1 : 1;
       setIsMoreLoading(true);
-      const { data } = await searchTodo({ q: debouncedSearchQuery, page: updateCurrentPage });
-      setSearchedResponse((prevData: string[]) => [...prevData, ...data.result]);
-      setIsNoMoreData(data.page * data.limit >= data.total);
+      const searchData = await getSearchData(updateCurrentPage, debouncedSearchQuery);
+      setSearchedResponse((prevData: string[]) => [...prevData, ...searchData.result]);
+      setIsNoMoreData(searchData.page * searchData.limit >= searchData.total);
       setIsMoreLoading(false);
     },
     [currentPage, setIsMoreLoading, setSearchedResponse, setIsNoMoreData]
